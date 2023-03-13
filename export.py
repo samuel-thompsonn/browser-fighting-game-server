7dd6be16e81e6baae4cba8cdb1cc2dc5bb9c8614
Samuel Thompson <sam.thompson356@gmail.com>
2023-03-12 23:33:27 -0400

    add zip exporting script

--- export.py
+++ export.py
@@ -1,17 +1,28 @@
 import zipfile
 import os
 
+zip_target_directories = [
+    'src'
+]
+
+zip_target_files = [
+    '.env',
+    'package.json',
+    'tscsonfig.json'
+]
+
 def zip_directory(path, zip_file):
     for root, dirs, files in os.walk(path):
         for file in files:
-            zip_file.write(os.path.join(root, file),
-                            os.path.relpath(os.path.join(root, file),
-                                            os.path.join(root, '..')))
+            print(os.path.join(root, file))
+            zip_file.write(os.path.join(root, file))
 
 def create_export_zip():
-    with zipfile.ZipFile('export.zip', 'w', zipfile.ZIP_DEFLATED) as zip_file:
-        zip_directory('src/', zip_file)
-
+    with zipfile.ZipFile('export/export.zip', 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        for directory in zip_target_directories:
+            zip_directory('src/', zip_file)
+        for file in zip_target_files:
+            zip_file.write(file)
         
 if __name__ == '__main__':
     create_export_zip()
